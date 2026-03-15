42ba8b3b126e8f8c8cd81f1fce9155cc31078e54
EMPI <adityamishra1337@gmail.com>
2026-03-15 16:44:25 +0530

    Updates

--- assets/Navbar-DGDeUzfp.js
+++ assets/Navbar-DGDeUzfp.js
@@ -1,4 +1,4 @@
-import{j,r as bt}from"./index-DR6a2VCQ.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j,r as bt}from"./index-oQVFjtap.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.14.2
  * https://gsap.com
  *
